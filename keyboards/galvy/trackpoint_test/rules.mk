64b272fc6067c784519f6dd72d2f13601e283aa7
galvy <galvinicer@gmail.com>
2022-09-05 12:34:54 -0400

    trackpoint

--- keyboards/galvy/trackpoint_test/rules.mk
+++ keyboards/galvy/trackpoint_test/rules.mk
@@ -8,13 +8,13 @@ BOOTLOADER = atmel-dfu
 #   change yes to no to disable
 #
 BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
-MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = yes        # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = no            # Enable N-Key Rollover
 BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
 AUDIO_ENABLE = no           # Audio output
+MOUSEKEY_ENABLE = yes # Mouse keys
 
 PS2_MOUSE_ENABLE = yes
 PS2_USE_USART = yes
